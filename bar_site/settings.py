d9e962f4ad71faf2fa2672ba1d7174051f798097
scinny91 <bellaaascinny@gmail.com>
2025-07-17 20:01:24 +0200

    aggiunto CSRF_TRUSTED_ORIGINS

--- bar_site/settings.py
+++ bar_site/settings.py
@@ -10,6 +10,7 @@ load_dotenv(dotenv_path=BASE_DIR / '.env')
 SECRET_KEY = 'django-insecure-secret-key'
 DEBUG = True
 ALLOWED_HOSTS = ['bar-production-3382.up.railway.app', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://bar-production-3382.up.railway.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
