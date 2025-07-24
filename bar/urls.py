23e685c5181f61e3d185bdb2e1f54d5fbb3ada30
scinny91 <bellaaascinny@gmail.com>
2025-07-24 23:50:37 +0200

    creata la view per gestire le giacenze

--- bar/urls.py
+++ bar/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
-    #path('', include('bar.prodotti.urls')),
+    path('', include('bar.prodotti.urls')),
     path('', include('bar.ordini.urls')),
 ]
